ffd071da09afb81cf6eb05cf0adad44f3f265bf0
naomieab <39127576+naomieab@users.noreply.github.com>
2020-05-04 22:21:47 +0300

    Add a new contract for Ownership asset

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,14 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+const OwnershipToken = artifacts.require("OwnershipToken");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  // deployer.deploy(ConvertLib);
+  // deployer.link(ConvertLib, MetaCoin);
+  // deployer.deploy(MetaCoin);
+
+  const name = "My car";
+  const symbol = "car";
+  const amount = 1;
+  deployer.deploy(OwnershipToken, name, symbol, amount);
 };

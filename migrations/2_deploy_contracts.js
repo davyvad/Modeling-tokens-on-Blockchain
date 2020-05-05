cfb9ca359c831fb1f971e40190e8a41a62b68479
david valensi <davidvalensi@MacBook-Air-de-David.local>
2020-05-05 10:38:57 +0300

    Add RentalToken.sol and add config in truffle-config.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+const RentalToken = artifacts.require("RentalToken");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  deployer.deploy(RentalToken, "rental1", "symb");
 };
